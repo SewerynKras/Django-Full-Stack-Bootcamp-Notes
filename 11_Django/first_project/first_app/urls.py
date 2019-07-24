accb2c4017deec3b21e522f3390258fc939ffe9b
Seweryn Kras <krasseweryn@gmail.com>
2019-07-24 22:13:46 +0200

    Django four, template inheritance

--- 11_Django/first_project/first_app/urls.py
+++ 11_Django/first_project/first_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from first_app import views
 
+
+app_name = "first_app"
+
 urlpatterns = [
-    path("", views.nested),
+    path("", views.nested, name='home_page'),
     path("img", views.image),
     path("webpages", views.webpages),
-    path("form", views.form),
-    path("users", views.users),
+    path("form", views.form, name='register'),
+    path("users", views.users, name='users'),
 ]
